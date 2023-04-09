35c309e52ca2c60f5fc04a4f675bfb4ef27b0753
Dapo Adedire <adedireadedapo19@gmail.com>
2023-04-09 18:53:36 +0100

    feat: working on the batch and single icon

--- components/main/download.tsx
+++ components/main/download.tsx
@@ -1,4 +1,6 @@
+
 import React, { useState } from "react";
+import Image from "next/image";
 
 export default function Download(): JSX.Element {
   const [downloadType, setDownloadType] = useState("single");
@@ -19,11 +21,10 @@ export default function Download(): JSX.Element {
           <button
             className={`rounded-full border px-4 
                     py-2
-                    ${
-                      downloadType === "single"
-                        ? "border-[#845eee94]"
-                        : "border-gray-300"
-                    }
+                    ${downloadType === "single"
+                ? "border-[#845eee94]"
+                : "border-gray-300"
+              }
                     `}
             onClick={() => handleDownloadType("single")}
           >
@@ -34,16 +35,15 @@ export default function Download(): JSX.Element {
           <button
             className={`relative rounded-full border px-4 py-2
            
-                        ${
-                          downloadType === "batch"
-                            ? "border-[#845eee94]"
-                            : "border-transparent"
-                        }
+                        ${downloadType === "batch"
+                ? "border-[#845eee94]"
+                : "border-transparent"
+              }
                         `}
             onClick={() => handleDownloadType("batch")
-          
-          }
-          disabled
+
+            }
+            // disabled
           >
             Batch Download
             <span
@@ -100,8 +100,19 @@ const SingleDownloadForm = () => {
       <div
         className="mb-2 w-full px-4"
       >
-        <h2 className="text-center text-lg font-medium">Download a single video.
-          
+        <div
+          className="flex items-center justify-center gap-4"
+        >
+          <div
+            className="rounded-md bg-[#F8E1FE] p-2"
+          >
+            <Image src='/images/single.png' height={45} width={45} alt="batch" />
+          </div>
+
+
+        </div>
+        <h2 className="text-center text-lg font-medium">
+          Single download supports one video at a time.
         </h2>
       </div>
       <div
@@ -109,7 +120,7 @@ const SingleDownloadForm = () => {
                     w-full
                     px-4
                     "
- >
+      >
         <input
           type="url"
           required
@@ -117,10 +128,10 @@ const SingleDownloadForm = () => {
           className="w-full rounded-md border border-gray-300 p-4
                         text-lg
                         font-medium
-                        focus:border-[#845eee94]
-                        focus:outline-transparent
-                        
                         shadow-[3px_rgb(0,0,0,0.2)]
+                        focus:border-[#845eee94]
+                        
+                        focus:outline-transparent
                         "
           onChange={handleYoutubeLink}
         />
@@ -178,14 +189,19 @@ const BatchDownloadForm = () => {
       <div
         className="mb-2 w-full px-4"
       >
-        <h2 className="text-center text-lg font-medium">Download multiple videos
-          {"  "}
-        <span
-        className="text-[#3500c794] underline"
+        <div
+        className="flex items-center justify-center gap-4"
         >
-            (max 4)
-        </span>
-        .
+          <div
+            className="rounded-md bg-[#F8E1FE] p-2"
+          >
+            <Image src='/images/batch.png' height={45} width={45} alt="batch" />
+          </div>
+
+       
+        </div>
+        <h2 className="my-3 text-center text-sm font-medium">
+          Batch download currently supports 3 video max.
         </h2>
       </div>
       <div className="flex w-full flex-col items-center justify-center gap-4">
